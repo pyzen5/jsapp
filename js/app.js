11e0b5a3bb409b87f55e7f89bc3573a4475ffa08
pyzen5 <pyzen55@gmail.com>
2020-04-15 17:34:41 +0530

    module

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-import person from './person.js';
-import { baseData } from './utility.js';
+import person from './person.js';   // default export - person, prs, etc.,
+import { baseData } from './utility.js';    // named export - {a}, {a as b}, * as c
 
 console.log(person.name);
 console.log(baseData)
