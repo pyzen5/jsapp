6abba6128a81c205dd3c5e38096dac42174a4fc6
pyzen5 <pyzen55@gmail.com>
2020-04-15 19:55:27 +0530

    error

--- js/spreadRest.js
+++ js/spreadRest.js
@@ -1,7 +1,8 @@
 const a = [1, 2, 3]
 
 const [a,b] = ['Max', '28'];
-{name} = {name:'k', age: 26}
+const aObj = {name:'k', age : 26};
+let {name} = aObj;
 const a = {name:'Max'};
 const b = a; a.name = 'Alex'; console.log(b.name)	// Alex
 const a = {name:'Max'};
